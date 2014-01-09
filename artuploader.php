f3e7805af86b65c4116b6eacf6209eb49b9d1e53
J Letts <entrepreneurj@gmail.com>
2014-01-09 14:36:11 +0000

    Switched to SimpleXML
    *Changed small amount of the image json format.
    *Still to do: Send curl requests to api.
    		Work out how to add images to article

--- artuploader.php
+++ artuploader.php
@@ -1,81 +1,90 @@
 <?php
 
-require_once("artuploader.class.php");
-
-$xmlDoc=getXML();
-$array = json_decode(json_encode(simplexml_load_string($xmlDoc)), TRUE); //Simplest way to turn into an array :)
-//var_dump($array);
-$Articles=array();
-// Since this is a POC and the xml is variable to change, we're gonna hack it :)
-
-foreach($array as $key =>$value){
-	if($key=="Article"){
-		$Articles=$value;
-		//$Articles[$key]["section"]=$array["@attributes"]["name"];
-
+$DIRPATH="examples";
+$xmlDocs=getXML(null, $DIRPATH);
+foreach($xmlDocs as $document)
+{
+	if (($document->getName())=="section"){
+		$section=$document;
+	} else {
+		//	$section=$document->section;
+		trigger_error("Incompatible file", E_USER_ERROR);
+		continue;
 	}
-}
-
-//file_put_contents("articles.txt",print($Articles));
-//file_put_contents("articles-inner.txt",print($Articles[0]));
-
-	var_dump($Articles);
-foreach($Articles as $k => &$v){
-
-//echo "ARTICLE\n";		
-	foreach($v as $key=>&$value){
-//echo "value";		
-//		var_dump( $value) ;
-//		var_dump($value['authors']);
-		//Change Author Field
-		if($key=="authors"){		
-			if(is_string($value)){//If there is only one author we need to JSONify by adding an "author" key
-				$auth_name=$value;
-				$value=array(array("author"=>$auth_name));
-			} elseif(is_array($value)) {// If author has a list of names we want to jsonify by giving them an "author" key
-				$author_array=array();
-				foreach($value as $name){	
-					$author_array[]=array("author"=>$name);
+	foreach($section->article as $art) {
+			//Add article category - default News
+		$art->addChild('category',(isset($section->attributes()->title) ? $section->attributes()->title : "news"));
+		//Add authors by username - if not username - default to felix - can be changed in before publish in engine
+		//Then again= this logic might be better behind the api? Dunno.
+		if( (bool) count($art->xpath("authors")) ){ // does the authors node exist?
+			foreach ($art->authors->xpath('author') as $uname){
+		
+				//if string contains a number we assume it is a username and convert it to felix
+				if (!(preg_match('#[\d]#',$uname) && $uname->getName()=="author")){
+					$art->authors->addChild('author','felix');
+					unset($uname);
+					continue;
 				}
-				$value=$author_array;
 			}
-
+		} else {
+			$art->addChild('authors');
+			$art->authors->addChild('author','felix');
 		}
 		//Change image field to base64 for api and nest
-		if($key=="images"){
-			if(array_has_key_attributes) //then must only be one image
+		$images=array("img1","img2");
+		foreach($images as &$value){
+			if(isset($art->{$value})){
+				if(isset($art->{$value}->image)){
+					
+					$filepath=$DIRPATH."/".$art->{$value}->image->attributes()->href;
+					if(file_exists($filepath)){
+						$finfo = finfo_open(FILEINFO_MIME_TYPE); //Sets finfo_open response to mime type of image
+						$mime=finfo_file($finfo, $filepath);
+						finfo_close($finfo);
+					
+						// TODO: Check to see if image is bigger than 3MB, if so, resize
+						$img_data=base64_encode(file_get_contents($filepath));
+						$filetype=explode(".",$filepath);
+						$img_ext=end($filetype);
+						$img_64='data:'.$mime.';base64,'.$img_data;
+						$art->{$value}->image=$img_64;
 
-		
-
-
+					} else {
+						unset($art->{$value});
+						continue;
+					}
+				}
+			}
 		}
-	}	
-unset($value);	
+		// TODO: Set date to 0700 on closest friday
+		$date = new DateTime();
+		$art->date=$date->getTimestamp();
+	}
 
-}	
-var_dump($Articles);
-file_put_contents("output.json",json_encode($Articles));
-
-
-
+	unset($section["title"]);
+	file_put_contents("output.json",json_encode($section,JSON_PRETTY_PRINT));
 
 
-function getXML($resource=null) {
+}
 
-//Temporary xml gathering for example
-	$path="examples"; // /path/to/directory
+	
+
+function getXML($resource=null,$DIRPATH=null) {
+	
+//Temporary xml gathering for example - normally would check to see if handed xml or directory to files
+	$path=$DIRPATH; // /path/to/directory
 	$xml_files=array();
-	$xmlHead='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';
-	$xml="";
+	/*$xmlHead='<?xml version="1.0" encoding="UTF-8" standalone="yes"?>';*/
+	$xml=array();
 	foreach (glob("$path/[0-9]*-[a-zA-z]*.xml") as $filepath) {
 		$file=file_get_contents($filepath);
 		//Tidying up the xml
-		$pattern=array("/<\?xml.*?>/",'/href="file:\/\/\/.[^>]*\.[a-zA-z]{3,4}"/', "/href_fmt/", '/\x{2029}/u', "/Text>/", "/Headline>/", "/Subheadline>/", "/Author>/", "/Image>/", "/<Image/" );
-		$replace=array("", "","href", "\n\n", "content>", "title>", "teaser>", "authors>", "images>", "<images");
-		$xml.=preg_replace($pattern, $replace, $file);
+		$pattern=array('/href="file:\/\/\/.[^>]*\.[a-zA-z]{3,4}"/', "/href_fmt/", '/\x{2029}/u', "/text>/", "/subheadline>/", "/headline>/", "/Author>/", "/Image>/", "/<Image/" );
+		$replace=array( "","href", "\n\n", "content>", "teaser>", "title>", "authors>", "images>", "<images");
+		$xml[]=simplexml_load_string(preg_replace($pattern, $replace, $file));
 		
 	}
-	return $xmlHead.$xml;
+	return $xml;
 }
 
 
